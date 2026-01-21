0e87298f4b4ed769af7985babd1c73f31a269706
Yossab_Samuel <153990503+Yossab55@users.noreply.github.com>
2026-01-21 12:43:41 +0200

    رسالة تشجيع

--- middleware/errorHandler.js
+++ middleware/errorHandler.js
@@ -1,7 +1,7 @@
 const errorHandler = (err, req, res, next) => {
     err.statusCode = err.statusCode || 500;
     err.message = err.message || 'Internal Server Error';
-
+// بصراحة معلم عاش بجد من الحاجات الي شوفتها حلوه انك عامل رسالة محترمة قوي
     res.status(err.statusCode).json({
         success: false,
         message: err.message,
